e9c72fd30e761dd2ac6acca69ee790c552ec9219
Emil Hammarberg <emilhammarberg@mailbox.org>
2026-05-27 19:01:04 +0200

    Format

--- potato.sh
+++ potato.sh
@@ -8,7 +8,7 @@ INTERACTIVE=true
 MUTE=false
 
 show_help() {
-	cat <<-END
+  cat <<-END
 	usage: potato [-s] [-m] [-w m] [-b m] [-h]
 	    -s: simple output. Intended for use in scripts
 	        When enabled, potato outputs one line for each minute, and doesn't print the bell character
@@ -24,82 +24,82 @@ END
 }
 
 play_notification() {
-	aplay -q /usr/lib/potato/notification.wav&
+  aplay -q /usr/lib/potato/notification.wav &
 }
 
 while getopts :sw:b:B:r:m opt; do
-	case "$opt" in
-		s)
-			INTERACTIVE=false
-			;;
-		m)
-			MUTE=true
-			;;
-		w)
-			WORK=$OPTARG
-			;;
-		b)
-			PAUSE=$OPTARG
-			;;
-		B)
-			LONG_PAUSE=$OPTARG
-			;;
-		r)
-			SESSIONS=$OPTARG
-			;;
-		h|\?)
-			show_help
-			exit 1
-			;;
-	esac
+  case "$opt" in
+    s)
+      INTERACTIVE=false
+      ;;
+    m)
+      MUTE=true
+      ;;
+    w)
+      WORK=$OPTARG
+      ;;
+    b)
+      PAUSE=$OPTARG
+      ;;
+    B)
+      LONG_PAUSE=$OPTARG
+      ;;
+    r)
+      SESSIONS=$OPTARG
+      ;;
+    h|\?)
+      show_help
+      exit 1
+      ;;
+  esac
 done
 
 time_left="%im left of %s "
 
 if $INTERACTIVE; then
-	time_left="\r$time_left"
+  time_left="\r$time_left"
 else
-	time_left="$time_left\n"
+  time_left="$time_left\n"
 fi
 
 CURR_SESSION=0
 while true
 do
-	((CURR_SESSION++))
-	for ((i=$WORK; i>0; i--))
-	do
-		printf "$time_left" $i "work"
-		sleep 1m
-	done
+  ((CURR_SESSION++))
+  for ((i=$WORK; i>0; i--))
+  do
+    printf "$time_left" $i "work"
+    sleep 1m
+  done
 
-	! $MUTE && play_notification
-	if $INTERACTIVE; then
-		read -d '' -t 0.001
-		echo -e "\a"
-		echo "Work over"
-		read
-	fi
+  ! $MUTE && play_notification
+  if $INTERACTIVE; then
+    read -d '' -t 0.001
+    echo -e "\a"
+    echo "Work over"
+    read
+  fi
 
-	if (($CURR_SESSION % $SESSIONS == 0))
-	then
-		for ((i=$LONG_PAUSE; i>0; i--))
-		do
-			printf "$time_left" $i "long pause"
-			sleep 1m
-		done
-	else
-		for ((i=$PAUSE; i>0; i--))
-		do
-			printf "$time_left" $i "pause"
-			sleep 1m
-		done
-	fi
+  if (($CURR_SESSION % $SESSIONS == 0))
+  then
+    for ((i=$LONG_PAUSE; i>0; i--))
+    do
+      printf "$time_left" $i "long pause"
+      sleep 1m
+    done
+  else
+    for ((i=$PAUSE; i>0; i--))
+    do
+      printf "$time_left" $i "pause"
+      sleep 1m
+    done
+  fi
 
-	! $MUTE && play_notification
-	if $INTERACTIVE; then
-		read -d '' -t 0.001
-		echo -e "\a"
-		echo "Pause over"
-		read
-	fi
+  ! $MUTE && play_notification
+  if $INTERACTIVE; then
+    read -d '' -t 0.001
+    echo -e "\a"
+    echo "Pause over"
+    read
+  fi
 done
